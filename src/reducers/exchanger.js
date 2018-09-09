809e5331f228a6092ded89ea4a962430a97265c0
Vladimir Sambilov <vladimir.sambilov@vlad-sam-mb.local>
2018-09-09 16:26:34 +0300

    backbone

---
+++ src/reducers/exchanger.js
@@ -0,0 +1,10 @@
+const initialState = {};
+
+export default function exchangerReducer(state = initialState, action) {
+    const { type, payload } = state;
+
+    switch (type) {
+        default:
+            return state;
+    }
+}
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import exchangerReducer from './exchanger';
+
+export default combineReducers({
+    exchangerReducer,
+});
